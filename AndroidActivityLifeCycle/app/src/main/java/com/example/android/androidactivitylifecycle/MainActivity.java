d412a04bbd81d79c6ca38ec0583f6da06f72e9c7
Kamlesh Rao <kamlesh.rao@gmail.com>
2018-08-16 13:44:17 +0530

    added lifecycle activities

--- AndroidActivityLifeCycle/app/src/main/java/com/example/android/androidactivitylifecycle/MainActivity.java
+++ AndroidActivityLifeCycle/app/src/main/java/com/example/android/androidactivitylifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.androidactivitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Inside onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Inside onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Inside onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Inside onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Inside onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Inside onDestroy", Toast.LENGTH_SHORT).show();
     }
 }
